7e7a0be2461dfdd47acef96c19785c7d26176bd9
Deming-Yang <ydmag1987@gmail.com>
2024-02-09 09:46:07 -0500

    update model

--- code/1 Driver.R
+++ code/1 Driver.R
@@ -29,6 +29,10 @@ sd.TC <- 1
 mean.d18Oi <- -1
 sd.d18Oi <- 1
 
+#meteoric source
+mean.dDi <- mean.d18Oi * 8 + 10
+sd.dDi <- 8
+
 # this is mean precipitation isotopes, OIPC not a good assumption?
 # Estimates for latitude 3.496°, longitude 36.0407°, altitude 365 m
 mean.d18Op <- 1.2 
@@ -41,16 +45,23 @@ sd.dD <- 0.3
 
 sd.d18O <- 0.1
 
+# how many time steps the vapor evolve around the mixing between atmosphere and evaporated lake 
+t <- 6 
+# The model is not sensitive to t, as it will converge towards a d18Ov that result in the LEL
+
+
 # parameters to monitor
-parameters <- c("TC", "rh", "x", "k", "dDp", "d18Op", "dDi","d18Oi",
-                "eD","e18O", "ekD", "ek18O", "dDA","d18OA", "dstarD", "dstar18O",
-                "dDv", "d18Ov","mD", "m18O", "pre.d18OL", "pre.dDL",
-                "intc", "sl")
+parameters <- c("TC", "f.ev", "k", "dDp", "d18Op", "dDi","d18Oi","f","dstarD", "dstar18O",
+                "eD","e18O", "ekD", "ek18O", "dstarD.ev", "dstar18O.ev","dDL.ev","d18OL.ev",
+                "dDv.ev", "d18Ov.ev","mD", "m18O", "pre.d18OL", "pre.dDL","dDA","d18OA",
+                "intc", "sl", "rh.ev", "dDL","d18OL", "rh","dDA.ev", "d18OA.ev",
+                "sl.ev", "intc.ev")
 
 # input data, including all environmental parameters and measured lake water isotopes
 dat = list( mean.TC = mean.TC, sd.TC = sd.TC, mean.d18Oi = mean.d18Oi,
+            mean.dDi = mean.dDi, sd.dDi = sd.dDi,
             mean.d18Op = mean.d18Op, sd.d18Op = sd.d18Op, mean.dDp = mean.dDp,
-            sd.dDp = sd.dDp, sd.d18Oi = sd.d18Oi,
+            sd.dDp = sd.dDp, sd.d18Oi = sd.d18Oi, t = t,
             lw.dD = lw.dD, lw.d18O = lw.d18O , N = N, sd.dD = sd.dD, sd.d18O = sd.d18O)
 
 #Start time
@@ -80,4 +91,6 @@ post.lw.evp.f$BUGSoutput$summary
 post.lw.evp.f$BUGSoutput$summary[,8]
 
 # check the density of parameters
-denplot(as.mcmc(post.lw.evp.f))
+denplot(as.mcmc(post.lw.evp.f), parms =c("TC", "rh.ev","dDi","d18Oi","k",
+                                          "dDA.ev","d18OA.ev", "dDv.ev","d18Ov.ev", "dDL.ev","d18OL.ev",
+                                          "f.ev","sl.ev", "intc.ev"))
--- code/2 Plots final.R
+++ code/2 Plots final.R
@@ -6,9 +6,17 @@ library(bayestestR)
 library(bayesplot)
 
 # compile posterior distribution for the variables
-post.d18Ov <- post.lw.evp.f$BUGSoutput$sims.list$d18Ov
+post.d18OA1 <- post.lw.evp.f$BUGSoutput$sims.list$d18OA[,1]
 
-post.dDv <- post.lw.evp.f$BUGSoutput$sims.list$dDv
+post.dDA1 <- post.lw.evp.f$BUGSoutput$sims.list$dDA[,1]
+
+post.d18OA <- post.lw.evp.f$BUGSoutput$sims.list$d18OA.ev
+
+post.dDA <- post.lw.evp.f$BUGSoutput$sims.list$dDA.ev
+
+post.d18Ov <- post.lw.evp.f$BUGSoutput$sims.list$d18Ov.ev
+
+post.dDv <- post.lw.evp.f$BUGSoutput$sims.list$dDv.ev
 
 post.d18Oi <- post.lw.evp.f$BUGSoutput$sims.list$d18Oi
 
@@ -18,15 +26,15 @@ post.d18Op <- post.lw.evp.f$BUGSoutput$sims.list$d18Op
 
 post.dDp <- post.lw.evp.f$BUGSoutput$sims.list$dDp
 
-post.dstar18O <- post.lw.evp.f$BUGSoutput$sims.list$dstar18O
+post.dstar18O <- post.lw.evp.f$BUGSoutput$sims.list$dstar18O[,t]
 
-post.dstarD <- post.lw.evp.f$BUGSoutput$sims.list$dstarD
+post.dstarD <- post.lw.evp.f$BUGSoutput$sims.list$dstarD[,t]
 
-post.intc <- post.lw.evp.f$BUGSoutput$sims.list$intc
+post.intc <- post.lw.evp.f$BUGSoutput$sims.list$intc.ev
 
-post.sl <- post.lw.evp.f$BUGSoutput$sims.list$sl
+post.sl <- post.lw.evp.f$BUGSoutput$sims.list$sl.ev
 
-post.x <- post.lw.evp.f$BUGSoutput$sims.list$x
+post.f <- post.lw.evp.f$BUGSoutput$sims.list$f.ev
 
 
 # maximum a posteriori estimate
@@ -44,24 +52,30 @@ intc.hdi025 <- hdi(post.intc, ci = 0.95)[[2]]
 intc.hdi975 <- hdi(post.intc, ci = 0.95)[[3]]
 
 # maximum a posteriori estimate
-x.map <- map_estimate(post.x)[[1]]
+f.map <- map_estimate(post.f)[[1]]
 
 # highest density interval, CI = 0.95
-x.hdi025 <- hdi(post.x, ci = 0.95)[[2]]
-x.hdi975 <- hdi(post.x, ci = 0.95)[[3]]
+f.hdi025 <- hdi(post.f, ci = 0.95)[[2]]
+f.hdi975 <- hdi(post.f, ci = 0.95)[[3]]
 
 # cloud plot for simulated sources 
 plot(x = lw.d18O, y = lw.dD, xlab = "d18O", ylab = "dD",
-     xlim = c(-20,30), ylim = c(-100,150), 
+     xlim = c(-10,10), ylim = c(-80,60), 
      col= alpha("cyan4", 0.5), pch = 16,
      main = "Simulated water isotopes, Lake Turkana evaporation model")
 abline(a = 10, b = 8, lwd = 2)
 
-points(x = post.d18Oi, y = post.dDi, col= alpha("green4", 0.01))
+points(x = post.d18Oi, y = post.dDi, col= alpha("green2", 0.01))
 
 points(x = post.d18Op, y = post.dDp, col= alpha("blue", 0.01))
 
-points(x = post.d18Ov, y = post.dDv, col= alpha("magenta4", 0.01))
+# evaporated lake
+points(x = post.d18Ov, y = post.dDv, col= alpha("magenta2", 0.01))
+
+points(x = post.d18OA1, y = post.dDA1, col= alpha("red", 0.01))
+
+# after mixing with evaporated lake
+points(x = post.d18OA, y = post.dDA, col= alpha("red4", 0.01))
 
 # there are a lot of uncertainties in the values of dstar
 # the values seem to depend on three things: 
@@ -76,21 +90,31 @@ abline(a = intc.hdi025, b = sl.hdi975, lwd = 1.5, col = "orange4", lty = 2)
 
 abline(a = intc.hdi975, b = sl.hdi025, lwd = 1.5, col = "orange4", lty = 2)
 
-legend(-20, 50, c("Lake water", "Inflow", "Precipitation", "Vapor", "Evp Lake"),
-       pch = c(16,1,1,1,1), col = c("cyan4", "green4", "blue", "magenta4", "orange"))
+legend(-10, 60, c("Lake water", "Inflow", "Precipitation", 
+                  "Evap. lake water", "Limiting delta",
+                  "Precip. Eq. air", "Air-Vapor mixture"),
+       pch = c(16,16,16,16,16,16,16), 
+       col = c("cyan4", "green2", "blue", "magenta2", "orange",
+                                        "red", "red4"))
 
 # bivariate density plots
 
-mcmc_hex(as.mcmc(post.lw.evp.f), pars = c("x", "rh"))
+mcmc_hex(as.mcmc(post.lw.evp.mod), pars = c("f.ev", "rh.ev"))
 
-mcmc_hex(as.mcmc(post.lw.evp.f), pars = c("x", "k"))
+# mcmc_hex(as.mcmc(post.lw.evp.mod), pars = c("f.ev", "k"))
+# 
+# mcmc_hex(as.mcmc(post.lw.evp.mod), pars = c("k", "rh.ev"))
 
-mcmc_hex(as.mcmc(post.lw.evp.f), pars = c("k", "rh"))
 
+# density plot for the posterior of f
+plot(density(post.f), xlim = c(0, 1), xlab = "f", ylab = "Density",
+     main = "Posterior distribution of f.ev")
+abline(v = f.map, lwd = 2)
+abline(v = f.hdi025, lwd = 1.5 , lty = 2)
+abline(v = f.hdi975, lwd = 1.5 , lty = 2)
 
-# density plot for the posterior of x
-plot(density(post.x), xlim = c(0.1, 0.8), xlab = "x", ylab = "Density",
-     main = "Posterior distribution of x")
-abline(v = x.map, lwd = 2)
-abline(v = x.hdi025, lwd = 1.5 , lty = 2)
-abline(v = x.hdi975, lwd = 1.5 , lty = 2)
+# plot relative humidity from before mixing to after mixing
+
+plot(density(post.rh.int), xlim = c(0.2, 1), xlab = "rh", ylab = "Density",
+     main = "Posterior distribution of rh", col = "blue")
+lines(density(post.rhev), col = "red")
